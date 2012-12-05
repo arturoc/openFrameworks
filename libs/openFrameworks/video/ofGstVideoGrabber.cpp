b30e59634a3665c3f1b2dcc9147492467070f77c
arturoc <arturo@openframeworks.cc>
2012-12-05 09:38:30 +0100

    ofGstVideoGrabber: remove framerate ifdefs for linux arm,
    problem was 125fps unsupported on ps3eye linux driver

--- libs/openFrameworks/video/ofGstVideoGrabber.cpp
+++ libs/openFrameworks/video/ofGstVideoGrabber.cpp
@@ -580,9 +580,7 @@ bool ofGstVideoGrabber::initGrabber(int w, int h){
 	if(format.fourcc==-1){
 		format_str_pipeline = "%s name=video_source device=%s ! "
 								 "%s,width=%d,height=%d "
-								 #ifndef TARGET_LINUX_ARM
 								 ",framerate=%d/%d "
-								 #endif
 								 "%s %s ";
 
 		pipeline_string=g_strdup_printf (
@@ -592,17 +590,15 @@ bool ofGstVideoGrabber::initGrabber(int w, int h){
 					      format.mimetype.c_str(),
 					      format.width,
 					      format.height,
-						  #ifndef TARGET_LINUX_ARM
+						  //#ifndef TARGET_LINUX_ARM
 					      format.choosen_framerate.numerator,
 					      format.choosen_framerate.denominator,
-					      #endif
+					      //#endif
 					      decodebin, scale);
 	}else{
 		format_str_pipeline = "%s name=video_source device=%s ! "
 								 "%s,format=\(fourcc\)%" GST_FOURCC_FORMAT ",width=%d,height=%d "
-								 #ifndef TARGET_LINUX_ARM
 								 ",framerate=%d/%d "
-								 #endif
 								 "%s %s ";
 
 		pipeline_string=g_strdup_printf (
@@ -613,18 +609,14 @@ bool ofGstVideoGrabber::initGrabber(int w, int h){
 					      GST_FOURCC_ARGS(format.fourcc),
 					      format.width,
 					      format.height,
-						  #ifndef TARGET_LINUX_ARM
 					      format.choosen_framerate.numerator,
 					      format.choosen_framerate.denominator,
-					      #endif
 					      decodebin, scale);
 	}
 #else
 	string format_str_pipeline = "%s name=video_source device=%s ! "
 			 "%s,width=%d,height=%d "
-			 #ifndef TARGET_LINUX_ARM
 			 ",framerate=%d/%d "
-			 #endif
 			 "%s %s ";
 
 	gchar* pipeline_string=g_strdup_printf (
@@ -634,10 +626,8 @@ bool ofGstVideoGrabber::initGrabber(int w, int h){
 		      format.mimetype.c_str(),
 		      format.width,
 		      format.height,
-			  #ifndef TARGET_LINUX_ARM
 		      format.choosen_framerate.numerator,
 		      format.choosen_framerate.denominator,
-		      #endif
 		      decodebin, scale);
 #endif
 
